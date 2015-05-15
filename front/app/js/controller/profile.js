664dd201db3674ecf9be1143f5a358540d0023e9
AbyChan <crystal.tyan@gmail.com>
2015-05-16 02:31:51 +0800

    update

--- front/app/js/controller/profile.js
+++ front/app/js/controller/profile.js
@@ -1,22 +1,25 @@
 angular.module('coffee.controllers')
     .controller('ProfileCtrl', [
         '$scope',
-      '$modal',
-      '$http',
+        '$modal',
+        '$http',
         function(
-          $scope,
-          $modal,
-        $http) {
+            $scope,
+            $modal,
+            $http) {
 
             $scope.animationsEnabled = true;
             $scope.coffeerName = 'Tyan';
             $scope.coffeer = {
                 avatar: '/img/default-avatar.png',
                 username: 'Tyan',
-                realname: 'Tyan Chan'
+                realname: 'Tyan Chan',
+                self_description: '沙漠的另一边会是什么，又一片沙漠罢了',
+                city: 'Wuhan',
+                country: 'China'
             };
 
-            $scope.open = function(size) {
+            $scope.edit_profile = function(size) {
                 var modalInstance = $modal.open({
                     animation: $scope.animationsEnabled,
                     templateUrl: 'editProfile.html',
@@ -30,21 +33,50 @@ angular.module('coffee.controllers')
                 });
             };
 
+          $scope.change_avatar = function(size) {
+            var modalInstance = $modal.open({
+              animation: $scope.animationsEnabled,
+              templateUrl: 'changeAvatar.html',
+              controller: 'ChangeAvatarCtrl',
+              size: size
+            });
+            modalInstance.result.then(function(avatar) {
+              $scope.coffeer.avatar = avatar;
+            }, function() {
+
+            });
+          };
+
         }
     ])
     .controller('EditProfileCtrl', [
         '$scope',
-      '$http',
-      '$modalInstance',
-      function($scope,
-               $http,
-               $modalInstance) {
-        $scope.ok = function () {
-          $modalInstance.close($scope.selected.item);
-        };
+        '$http',
+        '$modalInstance',
+        function($scope,
+            $http,
+            $modalInstance) {
+            $scope.submit  = function() {
+                $modalInstance.close($scope.selected.item);
+            };
 
-        $scope.cancel = function () {
-          $modalInstance.dismiss('cancel');
-        };
+            $scope.cancel = function() {
+                $modalInstance.dismiss('cancel');
+            };
+        }
+    ])
+    .controller('ChangeAvatarCtrl', ['$scope',
+        '$http',
+        '$modalInstance',
+        function($scope,
+            $http,
+            $modalInstance) {
+          $scope.ok  = function() {
+           //$modalInstance.close($scope.selected.item);
+          };
+
+          $scope.cancel = function() {
+            $modalInstance.dismiss('cancel');
+          };
         }
     ]);
