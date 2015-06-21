a156bed39577742562fb89ee93225121a194f8d7
AbyChan <crystal.tyan@gmail.com>
2015-06-21 20:25:29 +0800

    update

--- front/app/js/app.js
+++ front/app/js/app.js
@@ -1,44 +1,14 @@
 angular.module('coffee', [
-  'ui.router',
-  'textAngular',
-  'ui.bootstrap',
-  'coffee.controllers'])
+        'ui.router',
+        'coffee.controllers'
+    ])
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('dash', {
-              url: '/dash',
-              templateUrl: 'templates/dash.html',
-              controller: 'DashCtrl'
-            })
-        .state('login', {
-          url: '/login',
-          templateUrl: 'templates/login.html',
-          controller: 'LoginCtrl'
-        })
-        .state('logout', {
-          url: '/logout',
-          templateUrl: 'templates/logout.html',
-          controller: 'LogoutCtrl'
-        })
-        .state('signin', {
-          url: '/signin',
-          templateUrl: 'templates/signin.html',
-          controller: 'SigninCtrl'
-        })
-        .state('articles', {
-          url: '/articles',
-          templateUrl: 'templates/articles.html',
-          controller: 'ArticlesCtrl'
-        })
-        .state('profile', {
-          url: '/profile',
-          templateUrl: 'templates/profile.html',
-          controller: 'ProfileCtrl'
-        })
-        .state('editor', {
-          url: '/editor',
-          templateUrl: 'templates/editor.html',
-          controller: 'EditorCtrl'
-        });
-        $urlRouterProvider.otherwise('/dash');
+                url: '/home',
+                templateUrl: 'templates/home.html',
+                controller: 'HomeCtrl'
+            });
+
+        $urlRouterProvider.otherwise('/home');
     });
---
+++ front/app/js/controller/home.js
@@ -0,0 +1,7 @@
+angular.module('coffee.controllers')
+  .controller('HomeCtrl', [
+    '$scope', function(
+      $scope){
+
+
+    }]);
